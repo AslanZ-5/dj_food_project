25ca520034b045b51d773760f5d3a8de4eb4440a
Aslan <asl.zurabov@gmail.com>
2021-11-21 14:51:27 +0300

    rendering templates with django.template.loader.render_to_string

--- core/views.py
+++ core/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Article
+from django.template.loader import render_to_string
 import random
-a = random.randint(10,10000)
-h = f'''
-    <h1> Helloewe - {a} <h1>
-'''
+
+a = random.randint(10, 10000)
+
+
+# h = f'''
+#     <h1> Helloewe - {a}<h1>
+# '''
 
 
 def home(request):
-    return HttpResponse(h)
+    return HttpResponse(render_to_string('core/home.html', {'title': Article.objects.get(id=1).title,
+                                                            'content': Article.objects.get(id=1).content}))
