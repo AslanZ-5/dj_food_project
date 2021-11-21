e6454f99e8cf105cc689eff648a9a1b3acadc977
Aslan <asl.zurabov@gmail.com>
2021-11-21 13:59:43 +0300

    first view

--- core/views.py
+++ core/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+import random
+a = random.randint(10,10000)
+h = f'''
+    <h1> Helloewe - {a} <h1>
+'''
 
-# Create your views here.
+
+def home(request):
+    return HttpResponse(h)
