a7b4ea718ba2ca2bff992f3fdf8d08790d2e1f9e
Aslan <asl.zurabov@gmail.com>
2021-11-24 18:14:07 +0300

    AuthenticationForm

--- accounts/views.py
+++ accounts/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.shortcuts import redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm,AuthenticationForm
 
 
 def register_view(request):
@@ -13,22 +13,31 @@ def register_view(request):
             return redirect('login')
 
     form = UserCreationForm()
-    context = {'form':form}
+    context = {'form': form}
     return render(request, 'register.html', context=context)
+
 
 def login_view(request):
     if request.method == 'POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
+        form = AuthenticationForm(request,data=request.POST)
+        # username = request.POST.get('username')
+        # password = request.POST.get('password')
         # print(username, password)
-        user = authenticate(request, username=username, password=password)
-        if user is None:
-            context = {'error': 'password or username is incorrect'}
-            return render(request, 'login.html', context=context)
-        else:
-            login(request, user)
-            return redirect('/')
-    context = {}
+        # user = authenticate(request, username=username, password=password)
+        if form.is_valid():
+            user = form.get_user()
+            login(request,user)
+            return redirect('home')
+
+        # if user is None:
+        #     context = {'error': 'password or username is incorrect'}
+        #     return render(request, 'login.html', context=context)
+        # else:
+        #     login(request, user)
+        #     return redirect('/')
+    else:
+        form = AuthenticationForm(request)
+    context = {'form':form}
     return render(request, 'login.html', context=context)
 
 
