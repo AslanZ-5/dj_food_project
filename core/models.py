942cdce3d1e7c222d8e1d1c11447232ef15a7aee
Aslan <asl.zurabov@gmail.com>
2021-11-27 14:48:29 +0300

    signals post_save and pre_save

--- core/models.py
+++ core/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.utils.text import slugify
+from django.db.models.signals import post_save, pre_save
 
 
 class Article(models.Model):
@@ -11,7 +12,26 @@ class Article(models.Model):
     updated_date = models.DateTimeField(auto_now=True)
     publish = models.DateField(default=timezone.now)
 
-    def save(self, *args, **kwargs):
-        if self.slug is None:
-            self.slug = slugify(self.title)
-        super().save(*args, **kwargs)
+    # def save(self, *args, **kwargs):
+    #     if self.slug is None:
+    #         self.slug = slugify(self.title)
+    #     super().save(*args, **kwargs)
+
+
+def article_pre_save(sender, instance, *args, **kwargs):
+    print('pre_save')
+    print(args, kwargs)
+    instance.slug = slugify(instance.title)
+
+
+pre_save.connect(article_pre_save, sender=Article)
+
+
+def article_post_save(created, instance, *args, **kwargs):
+    print('post_save')
+    print(args, kwargs)
+    if created:
+        instance.slug = slugify(instance.title)
+    
+
+post_save.connect(article_post_save, sender=Article)
