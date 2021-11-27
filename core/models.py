957c35e02367a7a97fd25ee9b4fa143e14805403
Aslan <asl.zurabov@gmail.com>
2021-11-27 14:15:05 +0300

    overriding save method

--- core/models.py
+++ core/models.py
@@ -1,10 +1,17 @@
 from django.db import models
 from django.utils import timezone
+from django.utils.text import slugify
 
 
 class Article(models.Model):
     title = models.CharField(max_length=150)
+    slug = models.SlugField(max_length=150, null=True, blank=True)
     content = models.TextField()
     added_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now=True)
     publish = models.DateField(default=timezone.now)
+
+    def save(self, *args, **kwargs):
+        if self.slug is None:
+            self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
