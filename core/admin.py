2f9d46321687d7abb02105901403a4ec5fb0a561
Aslan <asl.zurabov@gmail.com>
2021-11-27 13:48:50 +0300

    changes in model

--- core/admin.py
+++ core/admin.py
@@ -3,7 +3,7 @@ from .models import Article
 
 
 class ArticleAdmin(admin.ModelAdmin):
-    list_display = ['id','title']
+    list_display = ['id','title','added_date','updated_date']
     search_fields = ['title','content']
 
 
