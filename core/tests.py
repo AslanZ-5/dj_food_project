681287ec0bb48da949562bc3be7ddd67d69d65b9
Aslan <asl.zurabov@gmail.com>
2021-12-06 11:25:51 +0300

    testing article search manager

--- core/tests.py
+++ core/tests.py
@@ -3,12 +3,12 @@ from .models import Article
 from django.utils.text import slugify
 from .utile import slugify_instance_title
 
+
 class ArticleTestCase(TestCase):
 
     def setUp(self):
         self.number_of_articles = 50
         for i in range(self.number_of_articles):
-
             Article.objects.create(title='hello world', content=f'ads{i}')
 
     def test_queryset_exists(self):
@@ -30,14 +30,20 @@ class ArticleTestCase(TestCase):
         for i in qs:
             title = slugify(i.title)
             slug = i.slug
-            self.assertEqual(slug,f'{title}-{i.id}')
+            self.assertEqual(slug, f'{title}-{i.id}')
 
     def test_slugify_instance_title(self):
         obj = Article.objects.all().last()
-        slugify_instance_title(obj,save=False)
-        self.assertEqual(obj.slug,f'{slugify(obj.title)}-{obj.id}')
+        slugify_instance_title(obj, save=False)
+        self.assertEqual(obj.slug, f'{slugify(obj.title)}-{obj.id}')
 
     def test_of_uniqueness_all_slugs(self):
-        slug_list = Article.objects.all().values_list('slug',flat=True)
+        slug_list = Article.objects.all().values_list('slug', flat=True)
         unique_slug_list = set(slug_list)
-        self.assertEqual(len(unique_slug_list),len(slug_list))
+        self.assertEqual(len(unique_slug_list), len(slug_list))
+
+    def test_article_search_manager(self):
+        qs = Article.objects.search(query='Hello World')
+        self.assertEqual(qs.count(),self.number_of_articles)
+        qs = Article.objects.search(query='Some thing else')
+        self.assertNotEqual(qs.count(), self.number_of_articles)
