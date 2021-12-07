7c17773f32290a4173cf4d222cff1568ff0d6dc5
Aslan <asl.zurabov@gmail.com>
2021-12-07 17:48:30 +0300

    adding ingredient form set in update view manually

--- recipes/forms.py
+++ recipes/forms.py
@@ -1,8 +1,14 @@
 from django import forms
-from .models import Recipe
+from .models import Recipe,Ingredient
 
 
 class RecipeForm(forms.ModelForm):
     class Meta:
         model = Recipe
         fields = ['name', 'description', 'direction']
+
+
+class IngredientForm(forms.ModelForm):
+    class Meta:
+        model = Ingredient
+        fields = ['name', 'quantity', 'unit']
--- recipes/views.py
+++ recipes/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
-from .forms import RecipeForm
-from .models import Recipe
+from .forms import RecipeForm, IngredientForm
+from .models import Recipe,Ingredient
 
 
 @login_required
@@ -39,10 +39,21 @@ def recipe_create_view(request):
 def recipe_update_view(request, id=None):
     obj = get_object_or_404(Recipe, id=id,user=request.user)
     form = RecipeForm(request.POST or None, instance=obj)
+
+    ing_forms = []
+    for ing_obj in obj.ingredient_set.all():
+        ing_forms.append(IngredientForm(request.POST or None,instance=ing_obj))
     context = {
         'form': form,
+        'form_2': ing_forms
     }
-    if form.is_valid():
-        form.save()
+    ing_forms_valid = all([form_ing.is_valid() for form_ing in ing_forms])
+    if form.is_valid() and ing_forms_valid:
+        parent = form.save(commit=False)
+        parent.save()
+        for ing in ing_forms:
+            child = ing.save(commit=False)
+            child.recipe = parent
+            child.save()
         context['message'] = 'Data is updated'
     return render(request,'recipes/create.html',context)
