1512bb5e80d2fb2c244af7e9e02b7c4ac7a30b1e
Aslan <asl.zurabov@gmail.com>
2021-12-07 09:08:31 +0300

    auto set quantity as a Float

--- recipes/admin.py
+++ recipes/admin.py
@@ -5,6 +5,7 @@ from .models import Recipe, Ingredient
 class RecipeIngredientInline(admin.StackedInline):
     model = Ingredient
     extra = 0
+    readonly_fields = ['quantity_as_float']
 
 
 class RecipeAdmin(admin.ModelAdmin):
@@ -14,5 +15,10 @@ class RecipeAdmin(admin.ModelAdmin):
     raw_id_fields = ['user']
 
 
+class IngredientAdmin(admin.ModelAdmin):
+    list_display = ['name','id']
+    readonly_fields = ['quantity_as_float']
+
+
 admin.site.register(Recipe, RecipeAdmin)
-admin.site.register(Ingredient)
+admin.site.register(Ingredient,IngredientAdmin)
--- recipes/models.py
+++ recipes/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.conf import settings
 from django.contrib.auth import get_user_model
 from .validator import validator_unit_of_measure
+from .utils import number_str_to_float
+
 
 class Recipe(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
@@ -13,12 +15,23 @@ class Recipe(models.Model):
     active = models.BooleanField(default=True)
 
 
+
 class Ingredient(models.Model):
     recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
     name = models.CharField(max_length=220)
     quantity = models.CharField(max_length=50)
-    unit = models.CharField(max_length=50,validators=[validator_unit_of_measure])
+    quantity_as_float = models.FloatField(blank=True, null=True)
+    unit = models.CharField(max_length=50, validators=[validator_unit_of_measure])
     description = models.TextField(blank=True, null=True)
     direction = models.TextField(blank=True, null=True)
     timestamp = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+
+    def save(self,*args,**kwargs):
+        qty = self.quantity
+        qty_as_float,qty_as_float_success = number_str_to_float(qty)
+        if qty_as_float_success:
+            self.quantity_as_float = qty_as_float
+        else:
+            self.quantity_as_float = None
+        super().save(*args,**kwargs)
