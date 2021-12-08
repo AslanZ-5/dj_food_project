842a381bb770e511e19874a2a0fd4f2041814994
Aslan <asl.zurabov@gmail.com>
2021-12-08 14:57:26 +0300

    form widgets

--- recipes/forms.py
+++ recipes/forms.py
@@ -1,8 +1,12 @@
 from django import forms
-from .models import Recipe,Ingredient
+from .models import Recipe, Ingredient
 
 
 class RecipeForm(forms.ModelForm):
+    error_css_class = 'rrrr-eerrrror'
+    required_css_class = 'ret-cl-tt'
+    name = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control','placeholder':'recipe name'}))
+    description = forms.CharField(widget=forms.Textarea(attrs={'class':'form-control','placeholder':'description'}))
     class Meta:
         model = Recipe
         fields = ['name', 'description', 'direction']
