d635d3c73ce45bec4ba72e5ffc706d0f58822f49
Aslan <asl.zurabov@gmail.com>
2021-12-23 20:32:14 +0300

    meals models ;

--- djsite/settings.py
+++ djsite/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'recipes',
     'django_htmx',
     'search',
+    'meals'
 
 ]
 
--- meals/models.py
+++ meals/models.py
@@ -1,3 +1,44 @@
 from django.db import models
+from django.conf import settings
+from recipes.models import Recipe
 
-# Create your models here.
+
+class MealStatus(models.TextChoices):
+    PENDING = "p", 'Pending'
+    COMPLETED = "c", "Completed"
+    EXPIRED = "e", "Expired"
+    ABORTED = "a", "Aborted"
+
+
+class MealQuerySet(models.QuerySet):
+    def by_user_id(self, user_id):
+        return self.filter(user_id=user_id)
+
+    def by_user(self, user):
+        return self.filter(user=user)
+
+    def pending(self):
+        return self.filter(status=MealStatus.PENDING)
+
+    def completed(self):
+        return self.filter(status=MealStatus.COMPLETED)
+
+    def expired(self):
+        return self.filter(status=MealStatus.EXPIRED)
+
+    def aborted(self):
+        return self.filter(status=MealStatus.ABORTED)
+
+
+class RecipeManager(models.Manager):
+    def get_queryset(self):
+        return MealQuerySet(self.model, using=self._db)
+
+
+class Meal(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=1, choices=MealStatus.choices, default=MealStatus.PENDING)
+    objects = RecipeManager()
